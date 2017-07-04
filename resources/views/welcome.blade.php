edfa24ed49963b7aa6051b4bc98cd246bf158874
chriselliot35 <christopher99elliot@gmail.com>
2017-07-04 15:09:22 -0400

    Update welcome.blade.php
    Changed background colour to ddd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #eee;
+                background-color: #ddd;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
